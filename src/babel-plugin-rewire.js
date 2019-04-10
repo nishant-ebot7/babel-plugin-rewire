fe8a586b422f86362ff6de2a68931b8b1e20225d
Nishant Tomer <nishant@e-bot7.com>
2019-04-10 14:13:16 +0200

    Remove rewirable nodeTypes

--- src/babel-plugin-rewire.js
+++ src/babel-plugin-rewire.js
@@ -33,10 +33,7 @@ module.exports = function({ types: t, template }) {
 		!(parent.type === 'ObjectTypeProperty') &&
 		!(parent.type === 'ClassMethod') &&
                 !(parent.type === 'TSTypeReference') && 
-                !(parent.type === 'TSTypeQuery') && 
                 !(parent.type === 'TSQualifiedName') && 
-                !(parent.type === 'CallExpression') && 
-                !(parent.type === 'TSDeclareFunction') && 
                 !(parent.type === 'TSExpressionWithTypeArguments') 
 	}
 
